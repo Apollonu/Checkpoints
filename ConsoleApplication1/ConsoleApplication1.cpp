8235085d056cb7ccf28bc459e9be11150b419bbf
Apollo <kingfrostalpha@gmail.com>
2024-08-23 12:04:40 -0400

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+/*  Program File Name: Checkpoint2.14 
+    Programmer: Harrison Hudgins
+    Date: 08/2024
+    Requirements:
+    Write a program that has the following character variables:
+    first, middle, and last. Store your initials in these variables
+    then display them on the screen
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
