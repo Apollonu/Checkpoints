fc6582cecc841abb4989e973dc9bc4195acde61a
Apollo <kingfrostalpha@gmail.com>
2024-09-20 07:45:00 -0700

    Update Chapter2-Ex10.cpp

--- Chapter2-Ex10.cpp
+++ Chapter2-Ex10.cpp
@@ -3,20 +3,23 @@
     Programmer: Harrison Hudgins
     Date: September 20, 2024
     Requirements:
-    A car holds 15 gallons of gasoline and can travel 375 miles before refueling.
-    Write a program that calculates the number of miles per gallon the car gets. Display the result on the screen.
+    Create a second branch that revises your code so that the user can enter the number of gallons and the miles driven.
 */
 
 #include <iostream>
 using namespace std;
 
-int gallons = 15;
-int miles = 375;
+int gallons;
+int miles;
 int MPG;
 
 int main()
 {
-    cout << "A car holds " << gallons << " gallons.\n" << "It can travel " << miles << " miles.\n\n";
+    cout << "Enter the amount of gallons a car has.\n";
+    cin >> gallons;
+    cout << "Enter the amount of miles the car can travel before refueling.\n";
+    cin >> miles;
+    cout << "\nThe car holds " << gallons << " gallons.\n" << "It can travel " << miles << " miles.\n\n";
     MPG = miles / gallons;
     cout << "The car has " << MPG << " miles per gallon.\n";
 }
