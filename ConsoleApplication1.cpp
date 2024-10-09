d2059d9f3b5af795c4f16b224752bf7e2452b2d4
Apollo <kingfrostalpha@gmail.com>
2024-10-09 06:57:44 -0700

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+/*  Project Name: Chapter 3 Exercise 17 - Math Tutor
+    File Name: ConsoleApplication1.cpp
+    Programmer: Harrison Hudgins
+    Date: October 9, 2024
+    Requirements:
+    Write a program that can be used as a math tutor for a young student.
+*/
+
+#include <iostream>
+using namespace std;
+
+int num1 = rand() % 1000;
+int num2 = rand() % 1000;
+int input;
+int total;
+int main()
+{
+    cout << "  " << num1 << "\n";
+    cout << "+ " << num2 << "\n";
+    cout << "--------\n";
+    cin >> input;
+    total = num1 + num2;
+    cout << total;
+}
