824185154f18e4a146e3037d3ccea9015e0ed95e
Apollo <kingfrostalpha@gmail.com>
2024-10-09 07:24:47 -0700

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,12 +1,12 @@
 /*  Project Name: Chapter 2 Exercise 18 - Energy Drinks
-    File Name: Chapter2-Ex18.cpp
+    File Name: ConsoleApplication1.cpp
     Programmer: Harrison Hudgins
     Date: September 25, 2024
     Requirements:
     A soft drink company recently surveyed 16,500 of its customers and found that approximately 15 percent of those surveyed purchase one or more energy drinks per week.
     Of those customers who purchase energy drinks, approximately 58 percent of them prefer citrus-flavored energy drinks. Write a program that displays the following:
-    • The approximate number of customers in the survey who purchase one or more energy drinks per week.
-    • The approximate number of customers in the survey who prefer citrus-flavored energy drinks.
+    â€¢ The approximate number of customers in the survey who purchase one or more energy drinks per week.
+    â€¢ The approximate number of customers in the survey who prefer citrus-flavored energy drinks.
 */
 
 #include <iostream>
